14989c164f458041b9a8a19fd7b2d18eaa4b0973
tina96pham <tina96pham@gmail.com>
2024-06-07 00:47:58 -0400

    Add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.scss';
+import Headers from './components/Headers/Headers';
 import HomePage from './pages/HomePage/HomePage';
 import TrackerPage from './pages/TrackerPage/TrackerPage';
 
 function App() {
   return (
     <BrowserRouter className="App">
+      <Headers/>
       <Routes>
         <Route path="/" element={<HomePage/>}/>
         <Route path="/tracker" element={<TrackerPage/>}/>
--- src/components/Headers/Headers.jsx
+++ src/components/Headers/Headers.jsx
@@ -1,11 +1,16 @@
 import "./Headers.scss"
+import logo from "../../assets/logo/REFINE - logo.svg"
+import { NavLink } from "react-router-dom";
 
 
 function Headers() {
   return (
     <div className="header">
-      <div className="header__title">
-        <h1>Rekindle</h1>
+      <img className="header__logo" src={logo} alt="Refine Logo" />
+      <NavLink  className="header__home" to="/">HOME</NavLink>
+      <div className="header__container">
+        <NavLink to="/log" className="header__link">My Progress</NavLink>
+        <NavLink to="/tracker" className="header__link">Waste Log</NavLink>
       </div>
     </div>
   );
